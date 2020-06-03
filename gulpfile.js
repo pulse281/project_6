9c99da867f7e46ba78d089d620b025bfacb19f4c
Denys <d229262@gmail.com>
2020-06-03 21:57:58 +0300

    last section consultation, all media

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,6 @@
-const gulp        = require('gulp');
+const gulp = require('gulp');
 const browserSync = require('browser-sync');
-const sass        = require('gulp-sass');
+const sass = require('gulp-sass');
 const cleanCSS = require('gulp-clean-css');
 const autoprefixer = require('gulp-autoprefixer');
 const rename = require("gulp-rename");
@@ -18,10 +18,10 @@ gulp.task('server', function() {
 
 gulp.task('styles', function() {
     return gulp.src("src/sass/**/*.+(scss|sass)")
-        .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
-        .pipe(rename({suffix: '.min', prefix: ''}))
+        .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
+        .pipe(rename({ suffix: '.min', prefix: '' }))
         .pipe(autoprefixer())
-        .pipe(cleanCSS({compatibility: 'ie8'}))
+        .pipe(cleanCSS({ compatibility: 'ie8' }))
         .pipe(gulp.dest("src/css"))
         .pipe(browserSync.stream());
 });
